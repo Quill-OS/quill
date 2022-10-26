Quill-OS/quill

commit e2c170b24992daed462a30017d62bf73b421e807

reader: Fix highlighting regression bug
Previously, the user could highlight text and unhighlight it, but unhighlighted text would have stayed highlighted until the user would have done a next-page-previous-page movement. This commit fixes this issue.

src/reader/reader.cpp
@@ -44,6 +44,7 @@ reader::reader(QWidget *parent) :
     global::reader::font = "Source Serif Pro";
     global::reader::fontSize = 10;
     global::reader::margins = 1;
+    global::reader::currentViewportText = "";
 
     ui->setupUi(this);
     ui->brightnessStatus->setFont(QFont("u001"));
@@ -518,6 +519,7 @@ reader::reader(QWidget *parent) :
         ui->graphicsView->hide();
         ui->graphicsView->deleteLater();
         ui->text->setText(epubPageContent);
+        global::reader::currentViewportText = ui->text->toHtml();
     }
     else if(is_pdf == true) {
         ui->text->hide();
@@ -540,6 +542,7 @@ reader::reader(QWidget *parent) :
     else {
         ui->graphicsView->hide();
         ui->graphicsView->deleteLater();
+        global::reader::currentViewportText = ui->text->toHtml();
         ui->text->setText(ittext);
     }
 
@@ -1014,6 +1017,7 @@ void reader::on_nextBtn_clicked()
             setup_book(book_file, split_total, false);
             ui->text->setText("");
             ui->text->setText(ittext);
+            global::reader::currentViewportText = ui->text->toHtml();
 
             pagesTurned = pagesTurned + 1;
             writeconfig_pagenumber(false);
@@ -1030,6 +1034,7 @@ void reader::on_nextBtn_clicked()
             setup_book(book_file, mupdf::epub::epubPageNumber, true);
             ui->text->setText("");
             ui->text->setText(epubPageContent);
+            global::reader::currentViewportText = ui->text->toHtml();
 
             pagesTurned = pagesTurned + 1;
             writeconfig_pagenumber(false);
@@ -1073,6 +1078,7 @@ void reader::on_previousBtn_clicked()
             setup_book(book_file, split_total, false);
             ui->text->setText("");
             ui->text->setText(ittext);
+            global::reader::currentViewportText = ui->text->toHtml();
 
             // We always increment pagesTurned regardless whether we press the Previous or Next button or not
             pagesTurned = pagesTurned + 1;
@@ -1108,6 +1114,7 @@ void reader::on_previousBtn_clicked()
             setup_book(book_file, mupdf::epub::epubPageNumber, true);
             ui->text->setText("");
             ui->text->setText(epubPageContent);
+            global::reader::currentViewportText = ui->text->toHtml();
 
             // We always increment pagesTurned regardless whether we press the Previous or Next button or not
             pagesTurned = pagesTurned + 1;
@@ -1308,10 +1315,8 @@ void reader::setTextProperties(int alignment, int lineSpacing, int margins, QStr
 
     QTextCursor cursor = ui->text->textCursor();
     textDialogLock = true;
-    // Kudos to Qt for not implementing the opposite of the following function /)_-)
-
     ui->text->setStyleSheet("QTextEdit { selection-background-color: white }");
-
+    // Kudos to Qt for not implementing the opposite of the following function /)_-)
     ui->text->selectAll();
     if(alignment == 0) {
         ui->text->setAlignment(Qt::AlignLeft);
@@ -1412,7 +1417,7 @@ void reader::setTextProperties(int alignment, int lineSpacing, int margins, QStr
     textDialogLock = false;
 
     // Highlighting
-    QString htmlText = ui->text->toHtml();
+    QString htmlText = global::reader::currentViewportText;
     QJsonObject jsonObject = getHighlightsForBook(book_file);
     int keyCount = 1;
     foreach(const QString& key, jsonObject.keys()) {
@@ -1826,7 +1831,6 @@ void reader::on_text_selectionChanged() {
                 global::reader::highlightAlreadyDone = false;
                 QJsonObject jsonObject = getHighlightsForBook(book_file);
                 QString htmlText = ui->text->toHtml();
-                qDebug() << htmlText << QString::number(global::reader::initialFontSize + global::reader::fontSize);
                 if(htmlText.contains("<span style=\" font-size:" + QString::number(global::reader::fontSize) + "pt; background-color:#bbbbbb;\">" + selected_text + "</span>") or htmlText.contains("<span style=\" background-color:#bbbbbb;\">" + selected_text + "</span>")) {
                     log("Highlight already done", className);
                     global::reader::highlightAlreadyDone = true;

src/functions.h
@@ -50,6 +50,7 @@ namespace global {
         static inline int initialFontSize = 6;
         inline int fontSize;
         inline int margins;
+        inline QString currentViewportText;
     }
     namespace kobox {
         inline bool showKoboxSplash;

commit db9dbb24ec3ad447ee6bdfb287e7977e7e5bd6fd

Reader: Fix major regression bug caused by commit e2c170b
This bug prevented the user from applying new font sizes, margins or line spacings to an opened book.
TODO: Fix ugly UI issue with highlighting

src/reader/reader.cpp
@@ -1297,6 +1297,9 @@ void reader::on_alignJustifyBtn_clicked()
 }
 
 void reader::setTextProperties(int alignment, int lineSpacing, int margins, QString font, int fontSize) {
+    // Don't try to improve this
+    // I have spent more time on it than I would care to admit
+
     // Alignment
     /*
      * 0 - Left
@@ -1305,31 +1308,8 @@ void reader::setTextProperties(int alignment, int lineSpacing, int margins, QStr
      * 3 - Justify
     */
 
-    // Don't try to improve this
-    // I have spent more time on it than I would care to admit
-
-    setLineSpacing(lineSpacing, false);
-    ui->lineSpacingSlider->setValue(lineSpacing);
-    setMargins(margins, false);
-    ui->marginsSlider->setValue(margins);
-
-    QTextCursor cursor = ui->text->textCursor();
-    textDialogLock = true;
-    ui->text->setStyleSheet("QTextEdit { selection-background-color: white }");
-    // Kudos to Qt for not implementing the opposite of the following function /)_-)
-    ui->text->selectAll();
-    if(alignment == 0) {
-        ui->text->setAlignment(Qt::AlignLeft);
-    }
-    else if(alignment == 1) {
-        ui->text->setAlignment(Qt::AlignHCenter);
-    }
-    else if(alignment == 2) {
-        ui->text->setAlignment(Qt::AlignRight);
-    }
-    else if(alignment == 3) {
-        ui->text->setAlignment(Qt::AlignJustify);
-    }
+    // Selection color
+    ui->text->setStyleSheet("QTextEdit { selection-background-color: white; color: white; }");
     // Font
     {
         if(font == "Crimson Pro") {
@@ -1411,10 +1391,6 @@ void reader::setTextProperties(int alignment, int lineSpacing, int margins, QStr
             writeFile(".config/04-book/font", font);
         }
     }
-    // Font size
-    ui->text->setFontPointSize(fontSize);
-    ui->text->setTextCursor(cursor);
-    textDialogLock = false;
 
     // Highlighting
     QString htmlText = global::reader::currentViewportText;
@@ -1427,17 +1403,44 @@ void reader::setTextProperties(int alignment, int lineSpacing, int margins, QStr
         }
         else {
             QString highlight = jsonObject.value(key).toString();
-            textDialogLock = true;
             if(htmlText.contains(highlight)) {
                 htmlText.replace(highlight, "<span style=\" background-color:#bbbbbb;\">" + highlight + "</span>");
             }
-            textDialogLock = false;
         }
         keyCount++;
     }
     htmlText.replace(QRegExp("font-family:'.*';"), "");
-    ui->text->setStyleSheet("QTextEdit { selection-background-color: black }");
     ui->text->setHtml(htmlText);
+
+    // Line spacing, margins
+    setLineSpacing(lineSpacing, false);
+    ui->lineSpacingSlider->setValue(lineSpacing);
+    setMargins(margins, false);
+    ui->marginsSlider->setValue(margins);
+
+    textDialogLock = true;
+    QTextCursor cursor = ui->text->textCursor();
+    // Kudos to Qt for not implementing the opposite of the following function /)_-)
+    ui->text->selectAll();
+    // Text alignment
+    if(alignment == 0) {
+        ui->text->setAlignment(Qt::AlignLeft);
+    }
+    else if(alignment == 1) {
+        ui->text->setAlignment(Qt::AlignHCenter);
+    }
+    else if(alignment == 2) {
+        ui->text->setAlignment(Qt::AlignRight);
+    }
+    else if(alignment == 3) {
+        ui->text->setAlignment(Qt::AlignJustify);
+    }
+    // Font size
+    ui->text->setFontPointSize(fontSize);
+    ui->text->setTextCursor(cursor);
+    // Selection color
+    ui->text->setStyleSheet("QTextEdit { selection-background-color: black; color: black; }");
+    textDialogLock = false;
 }
 
 void reader::menubar_show() {